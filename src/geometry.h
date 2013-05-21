5258d99bcfb60c164c908f65ea30e8b3d9674719
drasich <drasich@gmail.com>
2013-05-21 18:06:34 +0900

    geometry file

---
+++ src/geometry.h
@@ -0,0 +1,53 @@
+#ifndef __geometry__
+#define __geometry__
+#include "vec.h"
+#include "quat.h"
+#include <stdbool.h>
+
+typedef struct _Ray Ray;
+
+struct _Ray {
+  Vec3 Start;
+  Vec3 Direction;
+};
+
+typedef struct _Plane Plane;
+
+struct _Plane {
+  Vec3 Point;
+  Vec3 Normal;
+};
+
+typedef struct _Sphere Sphere;
+struct _Sphere {
+  Vec3 center;
+  float radius;
+};
+
+typedef struct _Repere Repere;
+struct _Repere {
+  Vec3 origin;
+  Quat rotation;
+};
+
+typedef struct _Triangle Triangle;
+struct _Triangle {
+  Vec3 v0;
+  Vec3 v1;
+  Vec3 v2;
+};
+
+
+typedef struct _Frustum Frustum;
+struct _Frustum {
+  float near;
+  float far;
+  Vec3 start;
+  Vec3 direction;
+  Vec3 up;
+  float fovy;
+  float fovx;
+};
+
+
+#endif
