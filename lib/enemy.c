c790673f1e167d9f5cadbce339e88ffdb125bee4
chris <chris@indefini.org>
2013-07-24 23:20:48 +0900

    shared component wip

---
+++ lib/enemy.c
@@ -0,0 +1,40 @@
+#include "component/enemy.h"
+#include "object.h"
+#include "property.h"
+#include <Eet.h>
+
+void
+enemy_init(Component* c)
+{
+}
+
+void
+enemy_update(Component* c, double dt)
+{
+  Enemy* e = c->data;
+  //c->object->angles.X += p->rotation_speed*dt;
+}
+
+static Eina_Inarray* 
+_enemy_properties()
+{
+  Eina_Inarray * iarr = create_property_set();
+
+  ADD_PROP(iarr, Enemy, name, EET_T_STRING);
+  ADD_PROP(iarr, Enemy, speed, EET_T_DOUBLE);
+
+  return iarr;
+}
+
+ComponentFuncs enemy_funcs = { enemy_init, enemy_update };
+
+Component*
+create_enemy()
+{
+  Enemy* e = calloc(1, sizeof *e);
+  Eina_Inarray* iarr = _enemy_properties();
+  e->speed = 20;
+
+  Component* c = create_component("enemy", enemy_funcs, e, iarr);
+  return c;
+}
---
+++ lib/enemy.h
@@ -0,0 +1,21 @@
+#ifndef __component_enemy__
+#define __component_enemy__
+#include "stdbool.h"
+#include "component.h"
+#include "object.h"
+
+typedef struct _Enemy Enemy;
+
+struct _Enemy
+{
+  const char* name;
+  double speed;
+  Object* object;
+};
+
+void enemy_init(Component* c);
+void enemy_update(Component* c, double dt);
+
+Component* create_enemy();
+
+#endif
