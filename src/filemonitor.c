dbb63cd43741a2f52ef6c000904d4958508723de
chris <chris@indefini.org>
2014-07-23 23:33:38 +0900

    image file monitor

--- src/filemonitor.c
+++ src/filemonitor.c
@@ -5,6 +5,7 @@
 void monitor_model(void *data, Ecore_File_Monitor *em, Ecore_File_Event event, const char *path)
 {
   printf("model event : %d, path %s \n", event, path);
+  //TODO
 }
 
 void monitor_shader(void *data, Ecore_File_Monitor *em, Ecore_File_Event event, const char *path)
@@ -24,7 +25,7 @@ void
 filemonitor_init(ResourceManager* rm)
 {
   ecore_file_init(); //TODO ecore_file_shutdown
-  const char* path = "test";
+  const char* path = "model";
   Ecore_File_Monitor * efm = ecore_file_monitor_add( path,
         monitor_model,
         NULL
@@ -36,7 +37,7 @@ filemonitor_init(ResourceManager* rm)
         rm
         );
 
-  path = "model";
+  path = "image";
   efm = ecore_file_monitor_add( path,
         monitor_texture,
         rm
